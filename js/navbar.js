d8a9f96d9fa2cb91594a7d64fcee9fa03f263109
Parth589 <73070478+Parth589@users.noreply.github.com>
2022-08-12 11:46:02 +0530

    dummy commit

--- js/navbar.js
+++ js/navbar.js
@@ -2,20 +2,19 @@ const toggleBtn = document.getElementById('hamburger-menu-trigger');
 const menu = document.querySelector('.primary-navigation');
 
 if (menu.getAttribute('aria-expanded') === 'true') {
-    toggleBtn.src = 'assets/icons/list(1).svg';
+    toggleBtn.src = 'assets/icons/x-lg(1).svg';
 }
 else {
-    toggleBtn.src = 'assets/icons/x-lg(1).svg';
-
+    toggleBtn.src = 'assets/icons/list(1).svg';
 }
 toggleBtn.addEventListener('click', () => {
     if (menu.getAttribute('aria-expanded') === 'true') {
         menu.setAttribute('aria-expanded', false);
-        toggleBtn.src = 'assets/icons/x-lg(1).svg';
+        toggleBtn.src = 'assets/icons/list(1).svg';
     }
     else {
         menu.setAttribute('aria-expanded', true);
-        toggleBtn.src = 'assets/icons/list(1).svg';
+        toggleBtn.src = 'assets/icons/x-lg(1).svg';
 
     }
 });
